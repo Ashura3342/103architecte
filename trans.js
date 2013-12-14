b6f649bc070f5a02ef900dfd016d412520d5c4a3
Ashura3342 <nathan.giallombardo@epitech.eu>
2013-12-14 03:26:41 +0100

    add style and display matrice

--- trans.js
+++ trans.js
@@ -3,14 +3,19 @@ function get_T(x, y)
 	return ([[1, 0, x], [0, 0, y], [0, 0, 1]]);
 }
 
-function get_H(x, y , z)
+function get_H(x, y)
 {
-	return ([[x, 0, 0], [0, y, 0], [0, 0, z]]);
+	return ([[x, 0, 0], [0, y, 0], [0, 0, 1]]);
 }
 
 function get_R(a)
 {
-	return ([[cos(a), -sin(a), 0], [sin(a), cos(a), 0], [0, 0, 1]]);
+	return ([[Math.cos(a), -Math.sin(a), 0], [Math.sin(a), Math.cos(a), 0], [0, 0, 1]]);
+}
+
+function get_S(a)
+{
+
 }
 
 function trans (mat1, mat2)
@@ -39,7 +44,10 @@ function trans (mat1, mat2)
 
 function init()
 {
-	alert("hello");
+	document.getElementById("r").innerHTML = get_R(90);
+	document.getElementById("t").innerHTML = get_T(2, 3);
+	document.getElementById("h").innerHTML = get_H(2, 2);
+	document.getElementById("th").innerHTML = trans(get_H(2, 2), get_T(2,3));
 }
 
 window.onload = function(){init();};
